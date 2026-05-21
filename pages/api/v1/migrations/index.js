1f9542c0342acb055bf8631db15d734c5cd378ca
FCorleth <zcorleth2005@gmail.com>
2026-05-21 08:56:52 -0300

    feat: adds `/api/v1/migrations endpoint`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,20 +1,44 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
+import database from "infra/database";
 
 export default async function migrations(request, response) {
   const method = request.method;
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    dir: join("infra", "migrations"),
+    direction: "up",
+    noLock: true,
+    verbose: true,
+    migrationsTable: "pgmigrations",
+  };
 
   try {
-    const migrations = await migrationRunner({
-      databaseUrl: process.env.DATABASE_URL,
-      dryRun: method === "GET" ? true : false,
-      dir: join("infra", "migrations"),
-      direction: "up",
-      noLock: true,
-      verbose: true,
-      migrationsTable: "pgmigrations",
-    });
-    response.status(200).json([migrations]);
+    if (method === "GET") {
+      const pendingMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: true,
+      });
+      await dbClient.end();
+      return response.status(200).json([pendingMigrations]);
+    }
+
+    if (method === "POST") {
+      const migratedMigrations = await migrationRunner({
+        ...defaultMigrationsOptions,
+        dryRun: false,
+      });
+
+      await dbClient.end();
+
+      if (migratedMigrations.length > 0) {
+        return response.status(201).json([migratedMigrations]);
+      }
+
+      return response.status(200).json([migratedMigrations]);
+    }
   } catch (error) {
     console.log(error);
     response.status(500).json({ error: error.message });
--- tests/integration/api/v1/migration/post.test.js
+++ tests/integration/api/v1/migration/post.test.js
@@ -1,10 +1,34 @@
+import database from "infra/database";
+
+beforeAll(cleanDatabase);
+
+async function cleanDatabase() {
+  await database.query("drop schema public cascade; create schema public;");
+}
+
 test("POST in /api/v1/migraitons should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations", {
     method: "POST",
   });
-
-  expect(response.status).toBe(200);
+  expect(response.status).toBe(201);
 
   const body = await response.json();
   expect(Array.isArray(body)).toBe(true);
+
+  const insideBody = body[0][0];
+  expect(insideBody.name).toBeDefined();
+  expect(insideBody.timestamp).toBeDefined();
+  expect(insideBody.path).toBeDefined();
+  expect(insideBody.path).toMatch(/infra.migrations/);
+
+  const secondResponse = await fetch(
+    "http://localhost:3000/api/v1/migrations",
+    {
+      method: "POST",
+    },
+  );
+  expect(secondResponse.status).toBe(200);
+
+  const secondResponseBody = await secondResponse.json();
+  expect(Array.isArray(secondResponseBody)).toBe(true);
 });
